b918d1b51004c2f6e542b7b8adc5690c7a52f01f
Jack <jakarahman007@gmail.com>
2020-03-08 20:02:17 +0700

    remove the timestamps column

--- database/migrations/2020_03_08_025651_create_post_tags_table.php
+++ database/migrations/2020_03_08_025651_create_post_tags_table.php
@@ -19,7 +19,6 @@ class CreatePostTagsTable extends Migration
             $table->foreign('post_id')->references('id')->on('posts')->onDelete('cascade');
             $table->unsignedBigInteger('tag_id');
             $table->foreign('tag_id')->references('id')->on('tags')->onDelete('cascade');
-            $table->timestamps();
         });
     }
 
