484575c42a0f12299e819b9d24cad3046e7eaf22
Jack <jakarahman007@gmail.com>
2020-03-09 08:05:53 +0700

    forget to add foreign key to author relation.

--- app/Post.php
+++ app/Post.php
@@ -20,7 +20,7 @@ class Post extends Model
      */
     public function author()
     {
-        return $this->belongsTo('App\User');
+        return $this->belongsTo('App\User', 'user_id');
     }
 
     /**
