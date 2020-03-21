1a343698f0f846fcf9343930f6bd996e50834954
Jack <jakarahman007@gmail.com>
2020-03-21 20:22:47 +0700

    add remember me func.

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -18,7 +18,7 @@
                         <div class="field">
                             <label for="email_or_username" class="label">Email / Username</label>
                             <div class="control has-icons-left">
-                                <input id="email_or_username" class="input" autofocus type="text" name="email_or_username" value="{{ old('email_or_username') }}" placeholder="Email / Username">
+                                <input id="email_or_username" class="input @error('email_or_username') is-danger @enderror" type="text" name="email_or_username" value="{{ old('email_or_username') }}" placeholder="Email / Username" autofocus>
                                 <span class="icon is-left">
                                     <i class="mdi mdi-email icon-in-form"></i>
                                 </span>
@@ -32,7 +32,7 @@
                         <div class="field">
                             <label for="password" class="label">Password</label>
                             <div class="control has-icons-left">
-                                <input id="password" type="password" class="input" type="text" name="password" placeholder="Password">
+                                <input id="password" type="password" class="input @error('password') is-danger @enderror" type="text" name="password" placeholder="Password">
                                 <span class="icon is-left">
                                     <i class="mdi mdi-lock icon-in-form"></i>
                                 </span>
@@ -42,11 +42,17 @@
                             @enderror
                         </div>
 
-                        <div class="is-inline-block">
+                        {{-- Remember Me --}}
+                        <label class="checkbox is-block">
+                            <input name="remember" type="checkbox">
+                            Remember me
+                        </label>
+
+                        <div class="is-inline-block" style="margin-top:15px;">
                             {{-- Register --}}
-                            <a href="{{route('register')}}" class="is-block has-text-primary" style="margin-top:10px;">Don't have an account ?</a>
+                            <a href="{{route('register')}}" class="is-block has-text-primary">Don't have an account ?</a>
                             {{-- Forget Password --}}
-                            <a href="{{route('password.request')}}" class="has-text-primary" style="margin-top:10px;">Forget Password ?</a>
+                            <a href="{{route('password.request')}}" class="has-text-primary">Forget Password ?</a>
                         </div>
                         {{-- Submit --}}
                         <button type="submit" class="button is-primary is-rounded is-pulled-right" style="margin-top:20px;">Login</button>
