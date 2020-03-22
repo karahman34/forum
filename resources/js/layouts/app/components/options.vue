993f44258d0bb0e12468668bc746cf009712f19d
Jack <jakarahman007@gmail.com>
2020-03-22 12:52:30 +0700

    add options for vue file

---
+++ resources/js/layouts/app/components/options.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="dropdown is-hoverable" :class="{'is-right': right}">
+    <!-- The Dots Trigger -->
+    <div class="dropdown-trigger">
+      <span class="options-dropdown-trigger"></span>
+      <span class="options-dropdown-trigger"></span>
+      <span class="options-dropdown-trigger"></span>
+    </div>
+
+    <!-- Dropdown Structure -->
+    <div class="dropdown-menu">
+      <div class="dropdown-content options-dropdown-content" role="menu">
+        <slot></slot>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    right: {
+      type: Boolean,
+      default: false,
+    },
+  },
+};
+</script>
--- resources/views/components/options.blade.php
+++ resources/views/components/options.blade.php
@@ -13,33 +13,3 @@
     </div>
   </div>
 </div>
-
-@push('css')
-  <style>
-    .options-dropdown-trigger {
-      display: inline-block;
-      background: grey;
-      width: 5px;
-      height: 5px;
-      border-radius: 50%;
-      margin-bottom: 3px;
-    }
-
-    .options-dropdown-content {
-      width: auto;
-      max-width: 100px;
-    }
-    .options-dropdown-content i {
-      margin-right: 3px;
-    }
-
-    .dropdown.is-right .dropdown-menu {
-      display: none;
-      justify-content: flex-end !important;
-    }
-
-    .dropdown.is-right:hover .dropdown-menu {
-      display: flex !important;
-    }
-  </style>
-@endpush
